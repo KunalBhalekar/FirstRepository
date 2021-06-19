77a0ab13918a19f2a2bf6929f4931dfbf0cfe13a
Bhalekar <910184@cognizant.com>
2021-06-19 15:53:09 +0530

    commit message

---
+++ Entitycrud/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entitycrud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            StudentContext context = new StudentContext();
+
+            Student s2 = context.Students.Find(1);
+            Console.WriteLine(s2.Name);
+            Console.ReadLine();
+
+         
+           
+        }
+    }
+}
---
+++ Entitycrud/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace Entitycrud
+{
+    class StudentContext :DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+
+}
